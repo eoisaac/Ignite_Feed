b13b90440d595b879f9bcb4a9e6c13eca6e22d39
Isaac <eoisaacc@gmail.com>
2022-06-04 00:06:16 -0300

    Finish Post component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './Post';
+import { Post } from './components/Post';
 
 export const App = () => {
   return (
@@ -11,7 +11,6 @@ export const App = () => {
       <Header />
 
       <div className={styles.wrapper}>
-
         <Sidebar />
 
         <main>
---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from './Post.module.css';
+
+export const Post = () => {
+	return (
+		<article className={styles.post}>
+			<header className={styles.header}>
+				<div className={styles.author}>
+					<img 
+						className={styles.avatar}
+						src="https://github.com/eoisaac.png" 
+					/>
+					<div className={styles.authorInfos}>
+						<strong className={styles.authorName}>
+							Isaac Santiago
+						</strong>
+						<span className={styles.authorTitle}>
+							Web Developer
+						</span>
+					</div>
+				</div>
+
+				<time className={styles.time} title="3th July at 11:27 PM" dateTime="2022-06-03 11:27:21 PM">
+					Publicado há 1h
+				</time>
+			</header>
+
+			<div className={styles.content}>
+				<p>Fala galeraa 👋</p>
+				<p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+ 				<p>
+					 <a href=""> jane.design/doctorcare </a>
+				</p>
+				<p> 
+					<a href="">#novoprojeto</a>{' '}
+					<a href="">#nlw</a>{' '}
+					<a href="">#rocketseat</a>
+				</p>
+				
+			</div>
+		</article>
+	);
+};
